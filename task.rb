fc926a4b96f1071f8893e88ef53087f2f4b5fa34
yuta <fantasista098@gmail.com>
2020-05-24 22:15:05 +0900

    Q3、Q4、Q７、Q8の修正、Q10の追記

--- task.rb
+++ task.rb
@@ -16,23 +16,25 @@ def q2
 
   # 以下に回答を記載
 
-  # array3 = array1 + array2
-  # p array3
-  p array1.concat(array2)
+  array3 = array1 + array2
+  p array3
+  # p array1.concat(array2)
 end
 
 def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
-  puts numbers.length
+  puts numbers.count(3)
+
+
 end
 
 def q4
   sports = ["サッカー", "フットサル", nil, "野球", "バスケ", nil, "バレー"]
 
   # 以下に回答を記載
-  sports.delete(nil)
+  sports.compact!
   # 以下は変更しないで下さい
   p sports
 end
@@ -59,7 +61,8 @@ def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-  array.map!{|item| item.to_i}
+  # array.map!{|item| item.to_i}
+    array.map!(& :to_i)
   # 以下は変更しないで下さい
   p array
 end
@@ -68,7 +71,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-  upper_case_programming_languages = programming_languages.map{|item| item.upcase}
+  programming_languages.map!(& :capitalize)
+  upper_case_programming_languages = programming_languages.map(& :upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -92,6 +96,7 @@ def q10
   else
     puts "まぁまぁ好きです"
   end
+  # foods.any?{|item| item == "うに"} ? puts "好物です" : puts "まぁまぁ好きです"
 end
 
 def q11
