1b1584475d4e9e46696f8a91dfb31871189364cc
yuta <fantasista098@gmail.com>
2020-05-27 19:58:53 +0900

    Q1０再修正

--- task.rb
+++ task.rb
@@ -91,10 +91,12 @@ def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
   
   # 以下に回答を記載
-  if foods.include?("うに")
-    puts "好物です"
-  else
-    puts "まぁまぁ好きです"
+  foods.each do |food|
+    if food.include? ("うに") 
+       puts "好物です" 
+    else
+       puts "まぁまぁ好きです"
+    end
   end
   
 end
